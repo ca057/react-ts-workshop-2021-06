acb4f9d2f5e61383b795cf1fb70f49dfa0c93616
Christian Ost <hallo@christianost.de>
2021-06-08 23:07:08 +0200

    extend counter to receive initial value as prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter initialValue={0} />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
