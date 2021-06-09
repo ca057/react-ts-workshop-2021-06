733505753bf5fbc3434d2d63d10d0ba1752bc101
Christian Ost <hallo@christianost.de>
2021-06-09 18:17:43 +0200

    fetch book from server

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./Book.css";
 
 interface Book {
@@ -5,14 +6,23 @@ interface Book {
   subtitle: string | null;
   numPages: number | null;
 }
-const book: Book = {
-  title: "My book",
-  subtitle: "This book is awesome!",
-  numPages: 23,
-};
+
+const isbn = "9781783983667";
 
 const BookDisplay = () => {
-  return (
+  const [book, setBook] = useState<Book>();
+
+  useEffect(() => {
+    async function fetchBook() {
+      const response = await fetch(`http://localhost:4730/books/${isbn}`);
+      const bookAsJson = await response.json();
+      setBook(bookAsJson);
+    }
+
+    fetchBook();
+  }, []);
+
+  return book ? (
     <div>
       <p className="book__title">
         {book.title}
@@ -21,7 +31,7 @@ const BookDisplay = () => {
       </p>
       <p className="book__pages">{book.numPages} pages</p>
     </div>
-  );
+  ) : null;
 };
 
 export default BookDisplay;
