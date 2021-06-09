e1d214b83385c3f212893d902fd4287220866318
Christian Ost <hallo@christianost.de>
2021-06-09 08:39:54 +0200

    add alert with price when clicking on a book

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,16 +1,22 @@
 import BookListItem from "./BookListItem";
 
 const list = [
-  { title: "A first Book", isbn: "1" },
-  { title: "Another awesome Book", isbn: "2" },
-  { title: "Learn React - the full book", isbn: "3" },
+  { title: "A first Book", isbn: "1", price: "19.99€" },
+  { title: "Another awesome Book", isbn: "2", price: "29.99€" },
+  { title: "Learn React - the full book", isbn: "3", price: "39.99€" },
 ];
 
 const BookList = () => {
   return (
     <ul>
       {list.map((book) => (
-        <BookListItem {...book} key={book.isbn} />
+        <BookListItem
+          {...book}
+          key={book.isbn}
+          onClick={() => {
+            window.alert(`"${book.title}" costs ${book.price}.`);
+          }}
+        />
       ))}
     </ul>
   );
--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -1,9 +1,10 @@
 interface BookListItemProps {
   title: string;
+  onClick: () => void;
 }
 
-const BookListItem: React.FC<BookListItemProps> = ({ title }) => {
-  return <li>{title}</li>;
+const BookListItem: React.FC<BookListItemProps> = ({ title, onClick }) => {
+  return <li onClick={onClick}>{title}</li>;
 };
 
 export default BookListItem;
